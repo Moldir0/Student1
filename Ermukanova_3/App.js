b02364ef75bd3f73d3e0dd013e34e7d8a6905cfb
Мөлдір <150453101+Moldir0@users.noreply.github.com>
2025-11-04 12:43:15 +0500

    Initialize React Native app with image carousel

---
+++ Ermukanova_3/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, View, Image, Button } from 'react-native';
+
+export default function App() {
+  const [page, setPage] = useState(0);
+
+  const images = [
+    require("./assets/plane1.jpg"),
+    require("./assets/plane2.jpg"),
+    require("./assets/plane3.jpg")
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={(e) => {
+          const pageIndex = Math.round(
+            e.nativeEvent.contentOffset.x / e.nativeEvent.layoutMeasurement.width
+          );
+          setPage(pageIndex);
+        }}
+        scrollEventThrottle={16}
+      >
+        {images.map((img, i) => (
+          <Image key={i} source={img} style={styles.image} />
+        ))}
+      </ScrollView>
+
+      {/* Индикатор */}
+      <View style={styles.dotContainer}>
+        {images.map((_, i) => (
+          <View key={i} style={[styles.dot, page === i && styles.activeDot]} />
+        ))}
+      </View>
+
+      {/* Button */}
+      <Button title="Брондау" onPress={() => alert("Брондау басталды ✅")} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  image: { width: 330, height: 200, margin: 10, borderRadius: 12 },
+  dotContainer: { flexDirection: "row", marginBottom: 10 },
+  dot: { width: 8, height: 8, borderRadius: 4, margin: 4, backgroundColor: "#bbb" },
+  activeDot: { backgroundColor: "#000" }
+});
